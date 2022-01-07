4d26359890c26440293193cc9c05128cb291927a
Christoph Herb <christoph.herb@jambit.com>
2022-01-07 18:39:43 +0100

    fix find files

--- nvim/lua/user/whichkey.lua
+++ nvim/lua/user/whichkey.lua
@@ -88,7 +88,7 @@ local mappings = {
   ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
   ["f"] = {
-    "<cmd>lua require().find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+    "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
     "Find files",
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
