99ebda2a583f8b01308af2e10355be57d356fda4
Christoph Herb <christoph.herb@jambit.com>
2022-01-09 22:54:47 +0100

    add mac install config

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DOTFILES_DIR=$(git rev-parse --show-toplevel)
+SCREENSHOT_PATH=~/screenshots
 
 # {{{util func
-prompt_install() {
+prompt_install() { #{{{
 	echo -n "$1 is not installed. Would you like to install it? (y/n) " >&2
 	old_stty_cfg=$(stty -g)
 	stty raw -echo
@@ -27,9 +28,9 @@ prompt_install() {
 			echo "I'm not sure what your package manager is! Please install $1 on your own and run this deploy script again. Tests for package managers are in the deploy script you just ran starting at line 13." 
 		fi 
 	fi
-}
+} #}}}
 
-check_for_sdkman() {
+check_for_sdkman() { #{{{
 	echo "Checking to see if sdkman is installed"
 	FILE=$HOME/.sdkman/bin/sdkman-init.sh
 	if ! [ -f "$FILE" ]; then
@@ -44,51 +45,38 @@ check_for_sdkman() {
 	else
 		echo "sdkman is installed."
 	fi
-}
+} #}}}
 
-check_for_basictex() {
-	echo "Checking to see if basictex is installed"
-	if ! [ -x "$(command -v latexmk)" ]; then
-		echo -n "basictex is not installed. Would you like to install it? (y/n) " >&2
+check_for_cask() { # {{{
+	echo "Checking to see if cask $1 is installed"
+  if brew list --cask $1 &>/dev/null; then
+		echo "$1 is installed"
+  else
+		echo -n "$1 is not installed. Would you like to install it? (y/n) " >&2
 		old_stty_cfg=$(stty -g)
 		stty raw -echo
 		answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 		stty $old_stty_cfg && echo
-		if echo "$answer" | grep -iq "^y" ;then
+		if echo "$answer" | grep -iq "^y" ; then
 		  if [ -x "$(command -v brew)" ]; then
-        # install tex
-        brew install --cask basictex
-        eval "$(/usr/libexec/path_helper)"
-
-        # update
-        sudo tlmgr update --self && sudo tlmgr update --all
-
-        # install font
-        curl https://www.tug.org/fonts/getnonfreefonts/install-getnonfreefonts > /tmp/install-getnonfreefonts
-        sudo texlua /tmp/install-getnonfreefonts
-        sudo getnonfreefonts --sys -a
-
-        # install dependencies
-        sudo tlmgr install latexmk ucs sectsty apacite titling blindtext todonotes texcount
+        brew install --cask $1 && echo "$1 is installed"
       else
-        echo "Brew not installed or you are not on mac env. Please install basictex manually."
+        echo "Brew not installed or you are not on mac env. Please install $1 manually."
       fi
 		fi
-	else
-		echo "basictex is installed."
 	fi
-}
+} #}}}
 
-check_for_software() {
+check_for_software() { #{{{
 	echo "Checking to see if $1 is installed"
 	if ! [ -x "$(command -v $1)" ]; then
 		prompt_install $1
 	else
 		echo "$1 is installed."
 	fi
-}
+} #}}}
 
-check_default_shell() {
+check_default_shell() { #{{{
 	if [ -z "${SHELL##*zsh*}" ] ;then
 			echo "Default shell is zsh."
 	else
@@ -103,7 +91,7 @@ check_default_shell() {
 			echo "Warning: Your configuration won't work properly. If you exec zsh, it'll exec tmux which will exec your default shell which isn't zsh."
 		fi
 	fi
-}
+} #}}}
 
 # $1 FROM, $2 TO
 setup_symlink() {
@@ -136,9 +124,15 @@ setup_zsh() { #{{{
 		if ! [ -x "$(command -v fzf)" ]; then
 				/usr/local/opt/fzf/install
 		fi
-		check_for_software xclip
+		check_for_softkare xclip
 		setup_symlink zsh/zshrc .zshrc
 		check_default_shell
+
+    # cht.sh
+    mkdir -p ~/.zsh.d/
+    curl https://cht.sh/:cht.sh > /usr/local/bin/cht.sh
+    chmod +x /usr/local/bin/cht.sh
+    curl https://cheat.sh/:zsh > ~/.zsh.d/_cht
 } #}}}
 
 setup_tmux() { #{{{
@@ -159,13 +153,38 @@ setup_tmuxinator() { #{{{
 } # }}}
 
 setup_latex() { #{{{
-  check_for_basictex
+    check_for_cask basictex
+
+    eval "$(/usr/libexec/path_helper)"
+
+    # update
+    sudo tlmgr update --self && sudo tlmgr update --all
+
+    # install font
+    curl https://www.tug.org/fonts/getnonfreefonts/install-getnonfreefonts > /tmp/install-getnonfreefonts
+    sudo texlua /tmp/install-getnonfreefonts
+    sudo getnonfreefonts --sys -a
+
+    # install dependencies
+    sudo tlmgr install latexmk ucs sectsty apacite titling blindtext todonotes texcount
 } # }}}
 
 setup_software() { #{{{
 		check_for_sdkman
-    check_for_software rust
+    check_for_software htop
+    check_for_software wget
+    check_for_software glances
+    check_for_software python@3.10
+    check_for_software rustup
     check_for_software go
+    check_for_software jq
+    check_for_software node
+
+    # provide public url for locally running server
+    check_for_software ngrok
+
+    # add gitignore
+    npx add-gitignore
 } #}}}
 
 setup_dotfiles() { #{{{
@@ -180,16 +199,54 @@ setup_kitty() { #{{{
 		setup_symlink kitty .config/kitty
 } #}}}
 
+setup_mac() { #{{{
+    # brew
+    echo "check if brew is installed.."
+		if ! [ -x "$(command -v brew)" ]; then
+      echo "install brew"
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+      echo "brew is installed"
+    fi
+
+    check_for_cask google-chrome
+    check_for_cask notion
+    check_for_cask scroll-reverser
+    check_for_cask tiles
+    check_for_cask discord
+    check_for_cask kitty
+    check_for_cask font-fira-code
+    check_for_cask bitwarden
+    check_for_cask docker
+    check_for_software speedtest-cli
+
+    # screenshots path
+    echo "change screenshot path to $SCREENSHOT_PATH"
+    mkdir -p $SCREENSHOT_PATH
+    defaults write com.apple.screencapture location $SCREENSHOT_PATH && killall SystemUIServer
+
+    # show hidden files
+    defaults write com.apple.finder AppleShowAllFiles YES
+    # show path bar
+    defaults write com.apple.finder ShowPathbar -bool true
+    # show status bar
+    defaults write com.apple.finder ShowStatusBar -bool true
+
+    # setup keyboard
+    echo "Install https://github.com/MickL/macos-keyboard-layout-german-programming"
+} # }}}
+
 git submodule update --init --recursive
 
 if [ "$1" = 'dotfiles' ]; then
     setup_dotfiles
     echo "dotfiles ready"
+elif [ "$1" = 'mac' ]; then
+    setup_mac
+    echo "mac ready"
 elif [ "$1" = 'kitty' ]; then
     setup_kitty
     echo "kitty ready"
-elif [ "$1" = 'keyboard' ]; then
-    echo "Install https://github.com/MickL/macos-keyboard-layout-german-programming"
 elif [ "$1" = 'zsh' ]; then
     setup_zsh
 		echo "Please log out and log back in for default shell to be initialized."
